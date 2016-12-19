a1f9aea9fb0713c9ebe3b6e284fbb6ae26b81115
Kyle Sentient <kylesentient@gmail.com>
2016-12-19 16:31:56 +0100

    Just styled Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,21 @@
 var React = require('react');
+var Link = require('react-router').Link;
 
 var Examples = () => {
     return (
-        <h3>Stateless Examples Route</h3>
+        <div>
+            <h1 className="text-center">Stateless Examples Route </h1>
+            <p> Here is a fucking example:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Constantine,DZ">Constantine, DZ</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Annaba,DZ">Annaba, DZ</Link>
+                </li>
+            </ol>
+        </div>
+
     );
 };
 
